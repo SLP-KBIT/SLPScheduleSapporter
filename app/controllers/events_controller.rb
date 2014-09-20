1d3ee531a71e2dc08d59183d835ac9bfaee4e9da
Club09 s09t214 OOKAWA Masahiro <clc.iguto@gmail.com>
2014-09-21 04:18:16 +0900

    イベントの登録を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,21 @@ class EventsController < ApplicationController
   def index
     @events = Event.order(:id).all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    render 'new' and return unless @event.save
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :start_at, :end_at, :content)
+  end
 end
